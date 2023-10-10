c2e3562c347c8ba346b7acb6bd8230875bc7c9b4
Lizandra <lizandragomess007@gmail.com>
2023-10-10 00:39:48 -0300

    adicionando o basico do arquivo 'rotas'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const rotas = require('./rotas')
-const cors = require('cors')
+const express = require('express');
+const rotas = require('./rotas');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use(rotas)
+app.use(express.json());
+app.use(cors());
+app.use(rotas);
 
-app.listen(3000)
+app.listen(3000);
---
+++ src/rotas.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const categoria = require('./controladores/categoria');
+const usuario = require('./controladores/usuario');
+const validaLogin = require('./intermediarios/validaLogin');
+
+const rotas = express();
+
+rotas.get('/categoria', categoria.listarCategoria);
+
+rotas.post('/usuario', usuario.cadastrarUsuario);
+
+rotas.post('/login', usuario.efetuarLogin);
+
+rotas.use(validaLogin);
+
+rotas.get('/usuario', usuario.obterPerfil);
+
+rotas.put('/usuario', usuario.atualizarPerfil);
+
+module.exports = rotas;
+
