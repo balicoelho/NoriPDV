a54350e37551a06c8c326f391d9d8f84c8776280
Barbara Coelho <balicoelho@gmail.com>
2023-10-11 10:52:21 -0300

    feat: Inclusão senhaToken no .env

--- src/controladores/usuario.js
+++ src/controladores/usuario.js
@@ -1,69 +1,76 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const knex = require('../database')
-const key = require('../key');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const knex = require("../database");
 
 const cadastrarUsuario = async (req, res) => {
-	const { nome, email, senha } = req.body
+  const { nome, email, senha } = req.body;
 
-	try {
+  try {
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
 
-        const senhaCriptografada = await bcrypt.hash(senha, 10);
+    const novoUsuario = await knex("usuarios")
+      .insert({
+        nome,
+        email,
+        senha: senhaCriptografada,
+      })
+      .returning(["id", "nome", "email"]);
 
-		const novoUsuario = await knex("usuarios").insert({
-			nome,
-			email,
-			senha: senhaCriptografada
-		}).returning(['id', 'nome', 'email'])
-
-        if (novoUsuario && novoUsuario.length > 0) {
-			return res.status(201).json(novoUsuario);
-		} else {
-			return res.status(400).json({ mensagem: 'Erro ao cadastrar o usuário' });
-		}
-
-	} catch (error) {
-		return res.status(400).json(error.message)
-	}
-}
+    if (novoUsuario && novoUsuario.length > 0) {
+      return res.status(201).json(novoUsuario);
+    } else {
+      return res.status(400).json({ mensagem: "Erro ao cadastrar o usuário" });
+    }
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 const efetuarLogin = async (req, res) => {
-    const { email, senha } = req.body;
+  const { email, senha } = req.body;
 
-    try {
+  try {
+    const usuario = await await knex("usuarios").where({ email }).first();
 
-        const usuario = await knex("usuarios").where({email})
-
-        if (usuario.rowCount < 1) {
-            return res.status(404).json({ mensagem: "Usuário e/ou senha inválido(s)." });
-        }
-    
-        const senhaValida = await bcrypt.compare(senha, usuario[0].senha);
-
-        if (!senhaValida) {
-            return res.status(404).json({ mensagem: "Usuário e/ou senha inválido(s)." });
-        }
-
-        const token = jwt.sign({ id: usuario[0].id }, key, { expiresIn: '10h' });
-
-        const { senha: _, ...usuarioLogado } = usuario[0];
-
-        return res.json({ usuario: usuarioLogado, token });
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ mensagem: `Erro interno do servidor. ${error.message}` });
+    if (!usuario) {
+      return res
+        .status(404)
+        .json({ mensagem: "Usuário e/ou senha inválido(s)." });
     }
-}
+
+    const senhaValida = await bcrypt.compare(senha, usuario.senha);
+
+    if (!senhaValida) {
+      return res
+        .status(404)
+        .json({ mensagem: "Usuário e/ou senha inválido(s)." });
+    }
+
+    const token = jwt.sign({ id: usuario.id }, process.env.SENHA_TOKEN, {
+      expiresIn: "10h",
+    });
+
+    const { senha: _, ...usuarioLogado } = usuario;
+
+    return res.json({ usuario: usuarioLogado, token });
+  } catch (error) {
+    return res.status(500).json({ mensagem: error.message });
+  }
+};
 
 const obterPerfil = async (req, res) => {
-    try {
-        return res.json(req.usuario);
-    } catch (error) {
-        res.status(401).json({ "mensagem": "Para acessar este recurso um token de autenticação válido deve ser enviado." })
-    }
-}
+  try {
+    return res.json(req.usuario);
+  } catch (error) {
+    res.status(401).json({
+      mensagem:
+        "Para acessar este recurso um token de autenticação válido deve ser enviado.",
+    });
+  }
+};
 
 module.exports = {
-	cadastrarUsuario, efetuarLogin, obterPerfil
-}
+  cadastrarUsuario,
+  efetuarLogin,
+  obterPerfil,
+};
--- src/intermediarios/validaToken.js
+++ src/intermediarios/validaToken.js
@@ -1,32 +1,39 @@
-const key = require('../key');
-const jwt = require('jsonwebtoken');
-const knex = require('../database')
+const jwt = require("jsonwebtoken");
+const knex = require("../database");
 
 const validaToken = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
+  try {
+    const token = req.headers.authorization.split(" ")[1];
 
-        const { id } = jwt.verify(token, key);
+    const { id } = jwt.verify(token, process.env.SENHA_TOKEN);
 
-        const rows = await knex('usuarios').where('id', id)
+    const rows = await knex("usuarios").where("id", id);
 
-        if (rows.length === 0) {
-            return res.status(401).json({ mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.' });
-        }
-
-        const usuario = {
-            id: rows[0].id,
-            nome: rows[0].nome,
-            email: rows[0].email
-        };
-
-        req.usuario = usuario;
-        next()
-
-    } catch (error) {
-        
-        return res.status(401).json({ mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.' });
+    if (rows.length === 0) {
+      return res
+        .status(401)
+        .json({
+          mensagem:
+            "Para acessar este recurso um token de autenticação válido deve ser enviado.",
+        });
     }
-}
+
+    const usuario = {
+      id: rows[0].id,
+      nome: rows[0].nome,
+      email: rows[0].email,
+    };
+
+    req.usuario = usuario;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({
+        mensagem:
+          "Para acessar este recurso um token de autenticação válido deve ser enviado.",
+      });
+  }
+};
 
 module.exports = validaToken;
